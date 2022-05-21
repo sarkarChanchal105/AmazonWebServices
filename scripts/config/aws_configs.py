c80743047679476bc5b9ac24c48de27d7f4b09f1
Chanchal Sarkar <csarkar@Chanchals-MBP-2.lan>
2022-05-20 18:32:45 -0700

    Changed

--- scripts/config/aws_configs.py
+++ scripts/config/aws_configs.py
@@ -1,6 +1,6 @@
 
-AWS_ACCESS_KEY_ID = 'AKIAJ4TBIBVII3KZGUJQ'  # put your IAM user's access key
-AWS_ACCESS_SECRET_KEY = 'Vs3eHirH4bJuC8F/nGAOHQoZ2U+LW4VNf5Q4eVBt' # put your IAM user's sectet key
-AWS_BUCKET_NAME = 'my-personal-bucket2017'  # put the name of the bucket
-FILE_NAME='Sample_upload.txt'
+AWS_ACCESS_KEY_ID = 'XXX''  # put your IAM user's access key
+AWS_ACCESS_SECRET_KEY = 'YYYYYYY'' # put your IAM user's sectet key
+AWS_BUCKET_NAME = 'my-personal-bucket2017-XXXXYYY'  # put the name of the bucket
+FILE_NAME='Sample_upload_22.txt'
 FOLDER_TO_CREATE='test_folder_to_create'
